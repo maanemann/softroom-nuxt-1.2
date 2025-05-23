04f67eace5aa373bed523ff3b69cd4d1fd097073
maanemann <hans.august@gmail.com>
2025-05-23 17:12:54 +0200

    init nuxt 3; install tailwind following official mini guide for nuxt

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <div class="bg-amber-300">
+    hey
+  </div> -->
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
