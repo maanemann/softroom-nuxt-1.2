29634e8eeeb154a249e26b86b1b9296c6cfb176c
maanemann <hans.august@gmail.com>
2025-06-11 14:09:15 +0200

    add app.config.ts populated w modified nuxt docs example content

---
+++ app.config.ts
@@ -0,0 +1,21 @@
+
+// § h1 : eksempel fra nuxt dokumentation, lav det om.
+  // § link : https://nuxt.com/docs/getting-started/configuration
+
+export default defineAppConfig({
+  title: 'softroom',
+  theme: {
+    dark: true,
+    colors: {
+      primary: '#000000'
+    }
+  }
+})
+
+
+// § h2 : "These variables are exposed to the rest of your application using the useAppConfig composable"
+
+// <script setup lang="ts">
+//   const appConfig = useAppConfig()
+// </script>
+
