2cd440c6b68e19bc22239667e429943e1583cb6a
maanemann <hans.august@gmail.com>
2025-06-12 13:01:24 +0200

    Add `ItemWrap` component styled w tailwind

---
+++ components/ItemWrap.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="
+      text-amber-800 text-5xl font-bold
+      bg-amber-200
+      m-2 rounded-3xl overflow-hidden
+    ">
+      <slot />
+    </div>
+  </div>
+</template>
+
