ba7ced0a7eef785bc3bf3c75f0b95adf4395e78c
maanemann <hans.august@gmail.com>
2025-06-12 12:43:52 +0200

    add default layout populated w test content

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+
+    <p
+      class="
+        absolute top-0 left-0
+        px-2 py-0.5
+        text-emerald-600 bg-teal-950
+        rounded-br-xl
+    ">
+
+      layoutttt
+
+    </p>
+
+    <slot />
+
+  </div>
+</template>
