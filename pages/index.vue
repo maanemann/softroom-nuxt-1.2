7a1e91185725129b582d3fe65d4ced3cd8015cd1
maanemann <hans.august@gmail.com>
2025-06-12 12:59:20 +0200

    add root index page w template using components & image not yet added

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template> <div>
+
+  <ItemWrap>
+    <TestTest />
+  </ItemWrap>
+
+  <ItemWrap>
+    <img src="~/public/img/hej.jpg" alt="hej">
+  </ItemWrap>
+
+  <!-- <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div> -->
+
+</div> </template>
