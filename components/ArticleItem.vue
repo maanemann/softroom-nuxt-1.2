eece5b55bf29b9ea8a9d9b7e7de0424d40b54b91
maanemann <hans.august@gmail.com>
2025-06-12 13:20:20 +0200

    add `ArticleItem` component for `main-slider`, receiving props in a ts-friendly manner

---
+++ components/ArticleItem.vue
@@ -0,0 +1,27 @@
+
+<script setup lang="ts">
+
+const props = defineProps<{
+  bgc?: string
+}>()
+
+  // console.log(props.title)
+</script>
+
+
+<template>
+
+  <!-- § TODO: omskriv værdier til grid variabler (i main.css) -->  
+  <figure
+    :class="`
+      bg-${props.bgc}
+      w-[calc(100vw-16rem)] h-full
+    `"/>
+    <!-- :style="{ backgroundColor: props.bgc }" -->
+    <!-- class="bg-rose-300/40 w-[calc(100vw-16rem)] h-full" -->
+
+</template>
+
+<style lang="css" scoped>
+
+</style>
