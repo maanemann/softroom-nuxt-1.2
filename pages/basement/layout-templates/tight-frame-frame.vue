ada6f8592b52ceca915bbb2811c03f2ca268650d
maanemann <hans.august@gmail.com>
2025-06-13 20:36:18 +0200

    add basic signature layout

--- pages/basement/layout-templates/tight-frame-frame.vue
+++ pages/basement/layout-templates/tight-frame-frame.vue
@@ -2,8 +2,24 @@
 </script>
 
 <template>
-  <div>
-    
+  <div class="bg-pink-950 min-w-screen min-h-screen grid">
+    <!-- § TODO: refaktorer klasser : -->
+    <div 
+      class="
+        bg-rose-900 min-w-[90vw] min-h-[90vh] m-2 rounded-3xl 
+        grid p-2
+      "
+    >
+      <p class="text-center text-5xl font-bold text-red-300 pb-2 px-2"> asd </p>
+      <!-- § TODO: fix roundig (tw tut) -->
+      <img
+        class="
+          w-full h-auto p-2 bg-red-300
+          rounded-t-sm rounded-b-2xl
+        "
+        src="~/public/img/hej.jpg" alt="ehh"
+      >
+    </div>
   </div>
 </template>
 
