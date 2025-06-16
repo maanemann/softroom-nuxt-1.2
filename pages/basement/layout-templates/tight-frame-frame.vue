faf2190ad3bb23f959e7f152d7fe9b53e4e440d4
maanemann <hans.august@gmail.com>
2025-06-16 13:26:07 +0200

    style `tight-frame-frame` to scroll appropriately; touch up colors; conclude initial plans for the layout

--- pages/basement/layout-templates/tight-frame-frame.vue
+++ pages/basement/layout-templates/tight-frame-frame.vue
@@ -2,19 +2,19 @@
 </script>
 
 <template>
-  <div class="bg-pink-950 min-w-screen min-h-screen grid">
+  <div class="bg-purple-950 w-screen h-screen grid p-2">
     <!-- § TODO: refaktorer klasser : -->
     <div 
       class="
-        bg-rose-900 min-w-[90vw] min-h-[90vh] m-2 rounded-3xl 
-        grid p-2
+        bg-fuchsia-900 w-full h-full rounded-3xl 
+        grid content-start p-2 overflow-scroll 
       "
     >
-      <p class="text-center text-5xl font-bold text-red-300 pb-2 px-2"> asd </p>
+      <p class="text-center text-5xl font-bold text-rose-300 pb-2 px-2"> asd </p>
       <!-- § TODO: fix roundig (tw tut) -->
       <img
         class="
-          w-full h-auto p-2 bg-red-300
+          w-full h-auto p-2 bg-rose-300
           rounded-t-sm rounded-b-2xl
         "
         src="~/public/img/hej.jpg" alt="ehh"
