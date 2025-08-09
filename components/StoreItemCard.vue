866df22926dcf00e6d48d3c4f40ca21bc378d576
maanemann <hans.august@gmail.com>
2025-08-09 19:15:04 +0200

    Add store item card component; Add responsive grid layout

---
+++ components/StoreItemCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+      class="
+        w-full h-64 rounded-sm
+        bg-stone-400
+      "
+    />
+</template>
+
+<style lang="css" scoped>
+
+</style>
--- pages/basement/instrumentbutik/index.vue
+++ pages/basement/instrumentbutik/index.vue
@@ -3,8 +3,19 @@
 </script>
 
 <template>
-  <div>
-    forside 
+  <div 
+    class="
+      grid grid-flow-row grid-cols-1 
+      p-2 gap-2 
+
+      sm:grid-cols-2 
+      md:grid-cols-3 
+      lg:grid-cols-4
+    "
+  >
+    <store-item-card 
+      v-for="i in 12" :key="i" 
+    />
   </div>
 </template>
 
