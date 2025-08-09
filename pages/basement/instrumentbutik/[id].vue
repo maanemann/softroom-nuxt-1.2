25904c21fe3b613f3d6de296cb670c2daf193a8c
maanemann <hans.august@gmail.com>
2025-08-09 16:21:14 +0200

    add routes for index (going to also cointain list view) and details view

---
+++ pages/basement/instrumentbutik/[id].vue
@@ -0,0 +1,16 @@
+
+<!-- se `/home/ms-binbows/_init/lokal/projekter/+0_tuts/nuxt 3, net ninja/fresh-create_fra-8/pages/products/[id].vue` -->
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    details view 
+  </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
---
+++ pages/basement/instrumentbutik/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    forside 
+  </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
