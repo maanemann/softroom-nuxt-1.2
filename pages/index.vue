259f69f5a1d457c290144d1bbc0d0feb8f99831f
maanemann <hans.august@gmail.com>
2026-02-23 21:17:12 +0100

    add first part of site map

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,22 @@
   </ItemWrap>
 
   <ItemWrap>
+    <ul class="pr-6 py-8 [&_li]:pl-6">
+      <li> basement
+        <ul>
+          <li><a href="/basement/v1">v1</a>
+            <ul>
+              <li> <a href="/basement/v1/colors"> colors </a> </li>
+              <li> <a href="/basement/v1/clip"> clip </a> </li>
+            </ul>
+          </li>
+          <li> ... </li>
+        </ul>
+      </li>
+    </ul>
+  </ItemWrap>
+
+  <ItemWrap>
     <!-- TODO: brug Image component ... -->
     <img src="~/public/img/hej.jpg" alt="hej">
   </ItemWrap>
